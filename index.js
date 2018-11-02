e800ef5cab65aa3215abed37b7dfcbd501c22ec2
Albangalo <Albert.chen.nz@gmail.com>
2018-11-02 10:39:04 -0500

    removed constipation

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const date = require('date-and-time')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var router = express.Router();
+
+router.get('/getTime', function(req, res){
+	let now = new Date();
+	res.json({'datetime' : date.format(now, 'YYYY/MM/DD HH:mm:ss')});
+});
+
+
+
+
+var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .use('/api', router)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
