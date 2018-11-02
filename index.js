2d62c7a544bfecaf1ca46a9b94dae85376987bb3
Madhuri Palagummi <madhuri.palagummi3@gmail.com>
2018-11-02 12:43:58 -0500

    database connection successfull

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const date = require('date-and-time')
 const path = require('path')
+var mysql = require('mysql');
+
 const PORT = process.env.PORT || 5000
 
 var router = express.Router();
@@ -10,14 +12,54 @@ router.get('/getTime', function(req, res){
 	res.json({'datetime' : date.format(now, 'YYYY/MM/DD HH:mm:ss')});
 });
 
+//b2632aa46c91e2  :9a689d80.   @us-cdbr-iron-east-01.cleardb.net/heroku_6220126aa0547cc?reconnect=true
+
+
+var connection = mysql.createConnection({
+  host     : 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'cknigijxoonxv3mw',
+  password : 'w6yd2p2pq2805iu9',
+  database : 'xwwsfos910k2ipxl'
+  // ssl: {
+  // 	'ca':'cleardbca.pem',
+  // 	'cert': 'b9ccd138d0a8a9-cert.pem',
+  // 	'key': 'b9ccd138d0a8a9-key.pem'
+  // }
+});
+
+var app = express();
 
 
 
-var app = express()
-  .use(express.static(path.join(__dirname, 'public')))
+connection.connect();
+
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection.end();
+
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .set('view engine', 'ejs');
+
+app.get('/db', function(request, response) {
+  response.send('Hello World!!!! HOLA MUNDOOOOO!!!');
+  connection.query('SELECT * from users', function(err, rows, fields) {
+      if (err) throw err;
+      console.log(rows);
+       //response.send('The solution is: ', rows[0].solution);
+    });
+});  
+
+app.get('/', (req, res) => res.render('pages/index'))
   .use('/api', router)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
